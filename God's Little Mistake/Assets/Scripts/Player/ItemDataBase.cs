1268dc09f7b80ab364f659217349e3af3560446a
LilliC2 <lillichuawiwat@gmail.com>
2023-07-03 17:55:09 +1000

    Adding in Scripts

---
+++ God's Little Mistake/Assets/Scripts/Player/ItemDataBase.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDataBase : Singleton<ItemDataBase>
+<<<<<<<< HEAD:God's Little Mistake/Assets/Scripts/Items/Databases and Classes/ItemDataBase.cs
+{ 
+    public Item[] itemDataBase;
+========
+{
+    public Item[] items;
+>>>>>>>> main:God's Little Mistake/Assets/Scripts/Player/ItemDataBase.cs
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
