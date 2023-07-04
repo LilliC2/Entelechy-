7990315461e6297ec2f197ce52999658c1fb003d
LilliC2 <lillichuawiwat@gmail.com>
2023-07-04 12:34:24 +1000

    Revived Sample Scene

--- God's Little Mistake/Assets/Scripts/Player/PlayerController.cs
+++ God's Little Mistake/Assets/Scripts/Player/PlayerController.cs
@@ -31,10 +31,6 @@ public class PlayerController : Singleton<PlayerController>
     public Vector3 target;
     bool projectileShot;
 
-    [Header("Items and Inventory")]
-
-    public Item[] playerInventory;  
-
     private void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
