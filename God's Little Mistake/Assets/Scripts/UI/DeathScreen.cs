0869ab4119e048d03fa4c0edb3eee5fa6282626b
TheRageofGod <spacedwarf1@gmail.com>
2023-07-22 01:31:27 +1000

    Death Screen

---
+++ God's Little Mistake/Assets/Scripts/UI/DeathScreen.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DeathScreen : GameBehaviour<DeathScreen> 
+{
+    public GameObject DeathScreenPanel;
+    public string currentScene;
+    void Update()
+    {
+        if(_PC.health <= 0)
+        {
+            Time.timeScale = 0f;
+            DeathScreenPanel.SetActive(true);
+        }
+    }
+
+    public void Respawn()
+    {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
+    }
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
