ba6d52a72e5f8b5434a1469950af6619bd1ff371
amer-palo <amer.190501@hotmail.com>
2023-07-23 21:09:18 +1000

    KnockBack Done

--- God's Little Mistake/Assets/Scripts/Traps/CactusTrap.cs
+++ God's Little Mistake/Assets/Scripts/Traps/CactusTrap.cs
@@ -6,6 +6,13 @@ public class CactusTrap : MonoBehaviour
 {
     public PlayerController playerController;
     public float damageOnCollision = 2f;
+    public float knockbackForce = 5f;
+    public float knockbackDuration = 0.5f;
+
+    private CharacterController characterController;
+    private Vector3 knockbackDirection;
+    private bool isKnockbackActive = false;
+    private float knockbackStartTime;
 
     private void Start()
     {
@@ -14,6 +21,29 @@ public class CactusTrap : MonoBehaviour
         if (playerObject != null)
         {
             playerController = playerObject.GetComponent<PlayerController>();
+            characterController = playerObject.GetComponent<CharacterController>();
+        }
+    }
+
+    private void Update()
+    {
+        if (isKnockbackActive)
+        {
+            // Calculate time since the knockback started
+            float timeSinceKnockback = Time.time - knockbackStartTime;
+
+            // Check if knockback duration has passed
+            if (timeSinceKnockback >= knockbackDuration)
+            {
+                isKnockbackActive = false;
+            }
+            else
+            {
+                // Apply knockback over time using Move
+                float knockbackProgress = timeSinceKnockback / knockbackDuration;
+                Vector3 currentKnockbackForce = Vector3.Lerp(knockbackDirection * knockbackForce, Vector3.zero, knockbackProgress);
+                characterController.Move(currentKnockbackForce * Time.deltaTime);
+            }
         }
     }
 
@@ -21,7 +51,17 @@ public class CactusTrap : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            // Apply damage to the player
             playerController.health -= damageOnCollision;
+
+            // Calculate knockback direction
+            knockbackDirection = collision.gameObject.transform.position - transform.position;
+            knockbackDirection.y = 0f; // Keep the knockback in the horizontal plane
+            knockbackDirection.Normalize();
+
+            // Activate knockback
+            isKnockbackActive = true;
+            knockbackStartTime = Time.time;
         }
     }
 }
