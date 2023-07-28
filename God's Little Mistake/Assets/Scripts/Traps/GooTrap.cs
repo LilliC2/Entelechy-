3abdfdfacd16d140dd582200a0bc7a6abae9d04f
amer-palo <amer.190501@hotmail.com>
2023-07-28 20:07:58 +1000

    Started Landmine Trap

--- God's Little Mistake/Assets/Scripts/Traps/GooTrap.cs
+++ God's Little Mistake/Assets/Scripts/Traps/GooTrap.cs
@@ -3,10 +3,9 @@ using System.Collections;
 
 public class GooTrap : MonoBehaviour
 {
-    public float gooSlowdownFactor = 0.5f; // The factor by which the player's speed will be reduced when in goo.
-    public float damageOverTime = 10f; // The amount of damage the player takes per second when in goo.
-    public float minSpeedInGoo = 1f; // The minimum speed the player can have while in the goo.
-
+    public float gooSlowdownFactor = 0.5f; 
+    public float damageOverTime = 1f; 
+    public float minSpeedInGoo = 1f;
     private bool isInGoo = false;
     private PlayerController playerController;
     private float originalSpeed;
@@ -15,11 +14,9 @@ public class GooTrap : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            // Player entered the goo trap area.
             playerController = other.GetComponent<PlayerController>();
             if (playerController != null)
             {
-                // Apply the goo effect on the player.
                 isInGoo = true;
                 originalSpeed = playerController.speed;
                 playerController.StartCoroutine(GooEffectCoroutine());
@@ -31,7 +28,6 @@ public class GooTrap : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            // Player exited the goo trap area.
             isInGoo = false;
         }
     }
@@ -40,27 +36,21 @@ public class GooTrap : MonoBehaviour
     {
         while (isInGoo)
         {
-            // Reduce player's speed when in goo.
             playerController.speed *= gooSlowdownFactor;
 
-            // Cap the speed to the minimum value.
             playerController.speed = Mathf.Max(playerController.speed, minSpeedInGoo);
 
-            // Deal damage over time to the player.
             playerController.health -= damageOverTime * Time.deltaTime;
 
-            // Check if player health is zero or below, which means the player is dead.
             if (playerController.health <= 0f)
             {
-                // Perform any actions needed when the player dies in goo (e.g., respawn, game over, etc.).
                 Debug.Log("Player died in the GooTrap!");
-                break; // Exit the loop if the player is dead.
+                break;
             }
 
             yield return null;
         }
 
-        // Restore the original speed when the player exits the goo trap area.
         playerController.speed = 4f;
     }
 }
