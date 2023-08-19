fc86a59bffdf4e85262780dc93e91f7ed69f2e5c
Lilli <lillichuawiwat@gmail.com>
2023-08-19 15:36:56 +1000

    Planning in GM

--- God's Little Mistake/Assets/Scripts/Universal/GameManager.cs
+++ God's Little Mistake/Assets/Scripts/Universal/GameManager.cs
@@ -5,12 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
+
+    public int dungeonLevel;
+
+
     public enum GameState
     {
         Playing, Pause, Iventory, Dead
     }
 
     public GameState gameState;
+
+
+    //generate room
+    //generate mini map
+
+    //find room end condition
+   
+    //if room is kill all enemies
+        /*spawn player in center of the room
+        */
+
+    //if room is find exit
+
+        /*spawn player at beginning of the room
+         * 
+         */
+
+  
+
+
+
+
+
+
 
 
   
