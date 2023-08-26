9736f0149635f292fd5b39149bfea1a4f9119360
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2023-08-26 19:54:12 +1000

    Hover stats now working except for sprites

--- God's Little Mistake/Assets/Scripts/UI/PauseFunctionality.cs
+++ God's Little Mistake/Assets/Scripts/UI/PauseFunctionality.cs
@@ -1,10 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PauseFunctionality : GameBehaviour
 {
-    public int slotNumber;
+    [Header("PlayerInventory")] //i dont know if this is needed
+    public int slotNumber1;
+    public int slotNumber2;
+    public int slotNumber3;
+    public int slotNumber4;
+    public int slotNumber5;
+    public int slotNumber6;
+
+    [Header("TopStats")]
+    public Sprite itemIcon;
+    public Sprite typeIcon;
+    public Sprite effectIcon;
+    public TMP_Text itemName;
+
+    [Header("CoreStats")]
+    public TMP_Text itemDamage;
+    public TMP_Text itemCritMultiplier;
+    public TMP_Text itemCritChance;
+    public TMP_Text itemFireRate;
+    public Sprite attackPill;
+    public Sprite attackIcon;
+    public TMP_Text attackText;
+    public Sprite rangePill;
+    public Sprite rangeIcon;
+    public TMP_Text rangeText;
+
+    [Header("Abilities")]
+    public TMP_Text abilityName;
+    public string abilityDescription = "";
+
+    [Header("Icons")]
+    public List<Sprite> itemTypeIcons;
+    public List<Sprite> itemEffectIcons;
+    public List<Sprite> itemAttackEffects;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +56,7 @@ public class PauseFunctionality : GameBehaviour
 
     public void PausingFucntion()
     {
-        _GM.OnPause();
+        _GM.OnPause(); //if needed to be use from the animations
     }
 
     public void SearchSlot()
@@ -27,11 +64,47 @@ public class PauseFunctionality : GameBehaviour
         //_AVTAR.slotsOnCanvas[slotNumber];
 
         //_PC.playerInventory.(slotNumber)
+
+        _PC.playerInventory.Clear();
     }
 
-    //public List<Item> SearchForItemMatch(Item item)
-    //{
-    //    List<Item> itemMatchInPlayerInven = new();
+    public void UpdateStats(PauseSlotStats _hoverItem)
+    {
+        itemName.text = _hoverItem.itemName;
+        itemDamage.text = _hoverItem.dmg.ToString();
+        itemCritMultiplier.text = _hoverItem.critX.ToString();
+        itemCritChance.text = _hoverItem.critChance.ToString();
+        itemFireRate.text = _hoverItem.fireRate.ToString();
+        itemIcon = _hoverItem.icon;
 
-    //}
+
+        //var matchItem = SearchForItemMatch(_hoverItem);
+
+        //if (matchItem != null)
+        //    print(matchItem[0]);
+        //else print("no match");
+    }
+
+    public void FixedUpdate()
+    {
+        
+    }
+
+    public List<Item> SearchForItemMatch(Item _hoverItem)
+    {
+        List<Item> itemMatchInPlayerInven = new();
+
+        print("Hover item is " + _hoverItem.itemName);
+
+        foreach (var item in _PC.playerInventory)
+        {
+            if (item.segment == _hoverItem.segment)
+            {
+                itemMatchInPlayerInven.Add(item);
+            }
+            var icon = item.icon;
+        }
+        return itemMatchInPlayerInven;
+
+    }
 }
---
+++ God's Little Mistake/Assets/Scripts/UI/PauseSlotStats.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseSlotStats : GameBehaviour
+{
+    public int slotNumber;
+
+    public string itemName;
+    public enum Category { Eyes, Mouth, Horns, Slappies, Punchies, Launchers, Legs, Crawlies, None }
+    public Category category;
+    public enum Segment { Head, Torso, Legs }
+    public Segment segment;
+    public enum ItemType { Primary, Secondary, Passive, Symbiote }
+    public ItemType itemType;
+
+    public enum AttackType { Cone, Straight }
+    public AttackType attackType;
+
+    public float currencyValue;
+
+    public bool active;
+
+    public float dmg;
+    public float dps;
+    public float fireRate;
+
+    public float critX; //crit dmg multipler
+    public float critChance;
+    public float range;
+
+    public bool projectile;
+    public float projectileSpeed;
+    public GameObject projectilePF;
+
+    public bool modifier;
+    public float modifierID;
+
+    public Sprite icon;
+    public GameObject avtarPrefab;
+    public GameObject avtarPrefabLeft;
+    public GameObject avtarPrefabRight;
+    public GameObject avtarPrefabBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MatchPlayerInventory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MatchPlayerInventory()
+    {
+        itemName = _PC.playerInventory[slotNumber].itemName;
+        dmg = _PC.playerInventory[slotNumber].dmg;
+        critChance = _PC.playerInventory[slotNumber].critChance;
+        critX = _PC.playerInventory[slotNumber].critX;
+        fireRate = _PC.playerInventory[slotNumber].fireRate;
+        projectile = _PC.playerInventory[slotNumber].projectile;
+        projectileSpeed = _PC.playerInventory[slotNumber].projectileSpeed;
+        icon = _PC.playerInventory[slotNumber].icon;
+
+
+    }
+}
