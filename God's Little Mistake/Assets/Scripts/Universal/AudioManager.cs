a309cbe5c7eeed8b50494072cfb53c707474c78d
Lilli <lillichuawiwat@gmail.com>
2023-10-01 11:15:30 +1100

    Item Pick Up

--- God's Little Mistake/Assets/Scripts/Universal/AudioManager.cs
+++ God's Little Mistake/Assets/Scripts/Universal/AudioManager.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class AudioManager : Singleton<AudioManager>
 {
-    AudioSource audioManagerSource;
+    [SerializeField]
+    AudioSource audioManagerSourcePlayerHurt;
+    [SerializeField]
+    AudioSource audioManagerSourceItem;
     public AudioClip[] playerHurtSounds;
 
     // Start is called before the first frame update
     void Start()
     {
-        audioManagerSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ItemPickUp()
     {
-        
+        audioManagerSourceItem.Play();
     }
 
     public void PlayerHurt()
     {
         var sound = playerHurtSounds[Random.Range(0, playerHurtSounds.Length)];
 
-        audioManagerSource.clip = sound;
-        audioManagerSource.Play();
+        audioManagerSourcePlayerHurt.clip = sound;
+        audioManagerSourcePlayerHurt.Play();
     }
 }
