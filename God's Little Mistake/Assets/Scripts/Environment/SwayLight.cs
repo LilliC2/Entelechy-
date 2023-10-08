b2435bd18f94fd7ab804ae429f515186f5ff42d1
Lilli <lillichuawiwat@gmail.com>
2023-10-08 21:25:05 +1100

    Added void to play scene

---
+++ God's Little Mistake/Assets/Scripts/Environment/SwayLight.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwayLight : MonoBehaviour
+{
+    [SerializeField]
+    float xMax;
+    [SerializeField]
+    float zMax;
+    [SerializeField]
+    float timeMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
