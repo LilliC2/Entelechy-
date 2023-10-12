e73cabf25f7d1f771d15c442996a5403b4a266b7
Lilli <107088307+LilliC2@users.noreply.github.com>
2023-10-12 13:53:25 +1100

    Fixed Parallax
    Script was change to only work for void floor. Edited to be able to accommodate both vertical and horizontal parallax

--- God's Little Mistake/Assets/Scripts/UI/ParralaxEffect.cs
+++ God's Little Mistake/Assets/Scripts/UI/ParralaxEffect.cs
@@ -11,6 +11,8 @@ public class ParralaxEffect : MonoBehaviour
     public float amountofParralax; //amount of scroll
     public Camera mainCamera;
 
+    public bool voidFloor;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,35 +27,59 @@ public class ParralaxEffect : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float distX = (mainCamera.transform.position.x * amountofParralax);
-        float distZ = (mainCamera.transform.position.z * amountofParralax);
-        //Vector3 pos = mainCamera.transform.position;
-        float tempX = mainCamera.transform.position.x * (1 - amountofParralax);
-        float tempZ = mainCamera.transform.position.z * (1 - amountofParralax);
-        //float distance = pos.x * amountofParralax;
+        if(voidFloor)
+        {
+            float distX = (mainCamera.transform.position.x * amountofParralax);
+            float distZ = (mainCamera.transform.position.z * amountofParralax);
+            //Vector3 pos = mainCamera.transform.position;
+            float tempX = mainCamera.transform.position.x * (1 - amountofParralax);
+            float tempZ = mainCamera.transform.position.z * (1 - amountofParralax);
+            //float distance = pos.x * amountofParralax;
 
-        transform.position = new Vector3(startingPosX + distX, transform.position.y, transform.position.z);
-        transform.position = new Vector3(transform.position.x, transform.position.y, startingPosZ + distZ); //Z because the spirtes are rotated 90 degrees
+            transform.position = new Vector3(startingPosX + distX, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y, startingPosZ + distZ); //Z because the spirtes are rotated 90 degrees
 
-        //never ending background
-        //X
-        if (tempX > startingPosX + (lengthOfSpriteX))
-        {
-            startingPosX += lengthOfSpriteX;
+            //never ending background
+            //X
+            if (tempX > startingPosX + (lengthOfSpriteX))
+            {
+                startingPosX += lengthOfSpriteX;
+            }
+            else if (tempX < startingPosX - (lengthOfSpriteX))
+            {
+                startingPosX -= lengthOfSpriteX;
+            }
+
+            //Z
+            if (tempZ > startingPosZ + (lengthOfSpriteZ))
+            {
+                startingPosZ += lengthOfSpriteZ;
+            }
+            else if (tempZ < startingPosZ - (lengthOfSpriteZ))
+            {
+                startingPosZ -= lengthOfSpriteZ;
+            }
         }
-        else if (tempX < startingPosX - (lengthOfSpriteX))
+        else
         {
-            startingPosX -= lengthOfSpriteX;
+            float dist = (mainCamera.transform.position.x * amountofParralax);
+            //Vector3 pos = mainCamera.transform.position;
+            float temp = mainCamera.transform.position.x * (1 - amountofParralax);
+            //float distance = pos.x * amountofParralax;
+
+            transform.position = new Vector3(startingPosX + dist, transform.position.y, transform.position.z);
+
+            //never ending background
+            if (temp > startingPosX + (lengthOfSpriteX))
+            {
+                startingPosX += lengthOfSpriteX;
+            }
+            else if (temp < startingPosX - (lengthOfSpriteX))
+            {
+                startingPosX -= lengthOfSpriteX;
+            }
         }
 
-        //Z
-        if (tempZ > startingPosZ + (lengthOfSpriteZ))
-        {
-            startingPosZ += lengthOfSpriteZ;
-        }
-        else if (tempZ < startingPosZ - (lengthOfSpriteZ))
-        {
-            startingPosZ -= lengthOfSpriteZ;
-        }
+      
     }
 }
