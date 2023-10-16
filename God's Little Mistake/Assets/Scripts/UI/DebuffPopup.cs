db427240bffd57c67b2ff49fae6c43f8541118c6
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2023-10-17 01:08:03 +1100

    debuff popup setup

--- God's Little Mistake/Assets/Scripts/UI/DebuffPopup.cs
+++ God's Little Mistake/Assets/Scripts/UI/DebuffPopup.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class DebuffPopup : MonoBehaviour
+public class DebuffPopup : GameBehaviour
 {
     public float durationTimer;
     public float duration;
     [SerializeField]
     Image fill;
+    public bool isActive;
 
+    private void Start()
+    {
+        fill.fillAmount = 1;
+        isActive = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        durationTimer -= Time.deltaTime;
+        //durationTimer -= Time.deltaTime;
 
-        fill.fillAmount = durationTimer / durationTimer;
+        //fill.fillAmount = durationTimer / durationTimer;
 
-        if (fill.fillAmount <= 0) Destroy(this.gameObject);
+        //if (fill.fillAmount <= 0) Destroy(this.gameObject);
+    }
+
+    //call this function with the duration of the debuff on current duration and max duration
+    void UpdateDebuffBar(float _maxDuration, float _currentDuration)
+    {
+        _currentDuration -= Time.deltaTime;
+        fill.fillAmount = _currentDuration / _maxDuration;
+        if(_currentDuration <= 0)
+        {
+            isActive = false;
+            Destroy(gameObject);
+        }
     }
 }
