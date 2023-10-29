f912ff5c367681ba1891205aa1f91121d11a0543
LilliC2 <lillichuawiwat@gmail.com>
2023-10-30 10:21:47 +1100

    Layering Script

---
+++ God's Little Mistake/Assets/Scripts/Layering.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layering : GameBehaviour
+{
+    [SerializeField]
+    SpriteRenderer[] spritesOnObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spritesOnObject = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z < _PC.transform.position.z)
+        {
+
+            //in front
+            foreach (var sprite in spritesOnObject)
+            {
+                sprite.sortingLayerID = SortingLayer.NameToID("Front");
+            }
+        }
+        else if(transform.position.z > _PC.transform.position.z)
+        {
+
+            //behind
+            foreach (var sprite in spritesOnObject)
+            {
+                sprite.sortingLayerID = SortingLayer.NameToID("Back");
+            }
+        }
+    }
+}
