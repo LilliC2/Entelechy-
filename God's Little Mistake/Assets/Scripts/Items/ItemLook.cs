617bb08972bc9cc78170bf1f3ecd38f4f0676824
LilliC2 <lillichuawiwat@gmail.com>
2023-10-30 09:45:46 +1100

    Adjust spit placement

--- God's Little Mistake/Assets/Scripts/Items/ItemLook.cs
+++ God's Little Mistake/Assets/Scripts/Items/ItemLook.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class ItemLook : GameBehaviour
 {
-
-    public GameObject firingPoint;
+    float directional;
 
     // Start is called before the first frame update
     void Start()
     {
-  
+        if (this.tag.Contains("Projectile"))
+        {
+            directional = _PC.directional.transform.eulerAngles.y;
+            transform.eulerAngles = new Vector3(45, 0, -directional);
+            //print("Direction " +-_PC.directional.transform.eulerAngles.y);
+
+        }
+
+
     }
 
     // Update is called once per frame
@@ -19,18 +26,9 @@ public class ItemLook : GameBehaviour
 
         //Rotates bullet so it faces the camera
         Vector3 rotate = new Vector3(45f, this.transform.rotation.y, this.transform.rotation.z);
-        
 
-        if(this.tag.Contains("Projectile"))
-        {
-            //transform.eulerAngles = rotate;
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
-            //print("direction " + direction);
-            transform.up = direction;
-        }
-        else
+
+        if (!this.tag.Contains("Projectile"))
         {
             transform.eulerAngles = rotate;
         }
