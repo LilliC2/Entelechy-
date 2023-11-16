43027b9935de074792faccdaf382bddbb855e7f7
Lilli <lillichuawiwat@gmail.com>
2023-11-16 11:24:05 +1100

    Rocket attack

--- God's Little Mistake/Assets/Scripts/Player/PlayerAttacks.cs
+++ God's Little Mistake/Assets/Scripts/Player/PlayerAttacks.cs
@@ -16,6 +16,7 @@ public class PlayerAttacks : Singleton<PlayerAttacks>
 
     bool projectileShot;
     bool projectileShot2;
+    bool projectileShot3;
 
 
     [Header("Sabertooth Projectile")]
@@ -65,6 +66,14 @@ public class PlayerAttacks : Singleton<PlayerAttacks>
                 SquitoAttack();
 
                 break;
+                
+            case 7: //Rocket Launcher Eyeball
+
+                print("Rocket");
+
+                RocketAttack();
+
+                break;
 
 
 
@@ -72,10 +81,16 @@ public class PlayerAttacks : Singleton<PlayerAttacks>
 
     }
 
+    public void RocketAttack()
+    {
+        BasicFireProjectileTorso(_IM.itemDataBase[7].projectilePF, _IM.itemDataBase[7].projectileSpeed, _IM.itemDataBase[7].firerate, _IM.itemDataBase[7].projectileRange);
+
+    }
+
     public void PeaShooterAttack()
     {
         _PE.peaShooterPS.Play();
-        BasicFireProjectile(_IM.itemDataBase[0].projectilePF, _IM.itemDataBase[0].projectileSpeed, _IM.itemDataBase[0].firerate, _IM.itemDataBase[0].projectileRange);
+        BasicFireProjectileHead(_IM.itemDataBase[0].projectilePF, _IM.itemDataBase[0].projectileSpeed, _IM.itemDataBase[0].firerate, _IM.itemDataBase[0].projectileRange);
         if (_FDM.leftFireFilling == false)
         {
             _FDM.leftHasFired = true;
@@ -87,7 +102,7 @@ public class PlayerAttacks : Singleton<PlayerAttacks>
     
     public void SquitoAttack()
     {
-        BasicFireProjectile(_IM.itemDataBase[4].projectilePF, _IM.itemDataBase[4].projectileSpeed, _IM.itemDataBase[4].firerate, _IM.itemDataBase[4].projectileRange);
+        BasicFireProjectileHead(_IM.itemDataBase[4].projectilePF, _IM.itemDataBase[4].projectileSpeed, _IM.itemDataBase[4].firerate, _IM.itemDataBase[4].projectileRange);
         if (_FDM.rightFireFilling == false)
         {
             _FDM.rightHasFired = true;
@@ -104,7 +119,7 @@ public class PlayerAttacks : Singleton<PlayerAttacks>
         if(returned)
         {
             returned = false;
-            BasicFireProjectile(_IM.itemDataBase[2].projectilePF, _IM.itemDataBase[2].projectileSpeed, _IM.itemDataBase[2].firerate, _IM.itemDataBase[2].projectileRange);
+            BasicFireProjectileHead(_IM.itemDataBase[2].projectilePF, _IM.itemDataBase[2].projectileSpeed, _IM.itemDataBase[2].firerate, _IM.itemDataBase[2].projectileRange);
             ExecuteAfterSeconds(1, () => returned = true);
             if (_FDM.leftFireFilling == false)
             {
@@ -144,19 +159,8 @@ public class PlayerAttacks : Singleton<PlayerAttacks>
 
     }
 
-    Vector3 CalculateTarget(Vector3 _originalPos, float _distance, float _angle)
-    {
-        // Convert the angle from degrees to radians
-        float angleInRadians = _angle * Mathf.Deg2Rad;
 
-        // Calculate the new position
-        float newX = _originalPos.x + _distance * Mathf.Cos(angleInRadians);
-        float newY = _originalPos.y + _distance * Mathf.Sin(angleInRadians);
-
-        return new Vector3(newX, newY, _originalPos.z);
-    }
-
-    public void BasicFireProjectile(GameObject _prefab, float _projectileSpeed, float _firerate, float _range)
+    public void BasicFireProjectileHead(GameObject _prefab, float _projectileSpeed, float _firerate, float _range)
     {
         Vector3 screenPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 cursorRay = Camera.main.ScreenPointToRay(Input.mousePosition).direction;
@@ -193,6 +197,49 @@ public class PlayerAttacks : Singleton<PlayerAttacks>
                 projectileShot = true;
 
                 ExecuteAfterSeconds(_firerate, () => projectileShot = false);
+            }
+            print("FIRE PROJECTILE");
+
+        }
+    }
+    
+    public void BasicFireProjectileTorso(GameObject _prefab, float _projectileSpeed, float _firerate, float _range)
+    {
+        Vector3 screenPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 cursorRay = Camera.main.ScreenPointToRay(Input.mousePosition).direction;
+        Vector3 flatAimTarget = screenPoint + cursorRay / Mathf.Abs(cursorRay.y) * Mathf.Abs(screenPoint.y - transform.position.y);
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            _PC.headFiringPoint.transform.LookAt(hit.point);
+
+            if (!projectileShot3)
+            {
+                print("Fire");
+                //particle system
+
+                //Spawn bullet and apply force in the direction of the mouse
+                //Quaternion.LookRotation(flatAimTarget,Vector3.forward);
+                GameObject bullet = Instantiate(_prefab, _PC.headFiringPoint.transform.position, _PC.headFiringPoint.transform.rotation);
+                bullet.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * _projectileSpeed);
+
+                //bullet.GetComponent<ItemLook>().firingPoint = _PC.headFiringPoint;
+                bullet.GetComponent<RangeDetector>().range = _range;
+                bullet.GetComponent<RangeDetector>().positionShotFrom = _PC.transform.position;
+
+                //knockbackActive = true;
+                //knockbackStartTime = Time.time;
+                Mathf.Clamp(bullet.transform.position.y, 0, 0);
+
+
+
+                //Controls the firerate, player can shoot another bullet after a certain amount of time
+                projectileShot3 = true;
+
+                ExecuteAfterSeconds(_firerate, () => projectileShot3 = false);
             }
             print("FIRE PROJECTILE");
 
