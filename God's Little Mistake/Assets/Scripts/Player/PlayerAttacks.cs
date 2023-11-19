6c0b83b7d9ba83096d1778247ee246e6dfeeb1ef
LilliC2 <lillichuawiwat@gmail.com>
2023-11-20 10:31:53 +1100

    Lob Fix

--- God's Little Mistake/Assets/Scripts/Player/PlayerAttacks.cs
+++ God's Little Mistake/Assets/Scripts/Player/PlayerAttacks.cs
@@ -6,10 +6,8 @@ public class PlayerAttacks : Singleton<PlayerAttacks>
 
 
     [Header("Lob Projectile")]
-    [SerializeField]
-    float angle;
-    [SerializeField]
-    float power;
+    float angle = -45;
+    float power = 5;
 
     [Header("Projectile")]
     public Vector3 target;
@@ -270,7 +268,9 @@ public class PlayerAttacks : Singleton<PlayerAttacks>
 
             print(_PC.directional.transform.forward);
 
-            _PC.torsoFiringPoint.transform.localEulerAngles = new(-angle, _PC.torsoFiringPoint.transform.localEulerAngles.y, _PC.torsoFiringPoint.transform.localEulerAngles.z);
+            _PC.torsoFiringPoint.transform.localEulerAngles = _PC.directional.transform.localEulerAngles;
+
+            _PC.torsoFiringPoint.transform.localEulerAngles = new(angle, _PC.torsoFiringPoint.transform.localEulerAngles.y, _PC.torsoFiringPoint.transform.localEulerAngles.z);
 
 
             bullet.GetComponent<Rigidbody>().AddForce(power * _PC.torsoFiringPoint.transform.forward, ForceMode.Impulse);
