8e68e2f5286deee0d7d6cdefcbe20dc295cea60f
Lilli <lillichuawiwat@gmail.com>
2023-11-23 10:43:32 +1100

    Peashooter

--- God's Little Mistake/Assets/Scripts/Player/Projectiles/RangeDetector.cs
+++ God's Little Mistake/Assets/Scripts/Player/Projectiles/RangeDetector.cs
@@ -7,14 +7,44 @@ public class RangeDetector : GameBehaviour
     public float range;
     public Vector3 positionShotFrom;
 
+    public ParticleSystem endOfRangePS;
+
+    bool playedPS;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(Vector3.Distance(transform.position, positionShotFrom) >= range)
         {
-            //print("Range is " + range + ". Out of range at (PC)" + Vector3.Distance(transform.position, _PC.transform.position));
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            if (endOfRangePS != null)
+            {
+                if(!playedPS)
+                {
+                    playedPS = true;
+                    endOfRangePS.Play();
+
+                    ExecuteAfterSeconds(endOfRangePS.main.duration, () => Destroy(gameObject));
+                }
+
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+            print("Range is " + range + ". Out of range at (PC)" + Vector3.Distance(transform.position, _PC.transform.position));
             //print("Range is " + range + ". Out of range at (pos shot) " + Vector3.Distance(transform.position, positionShotFrom));
-            Destroy(gameObject);
+            //ExecuteAfterFrames(3, () => Destroy(gameObject));
+
         }
 
     }
