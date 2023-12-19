75384c49426e168fbd196a6159b180d856b3b60e
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2023-12-19 17:38:14 +1100

    working template for instruction

--- God's Little Mistake/Assets/Scripts/UI/Terrystruction.cs
+++ God's Little Mistake/Assets/Scripts/UI/Terrystruction.cs
@@ -39,17 +39,14 @@ public class Terrystruction : MonoBehaviour
         switch (pageNumber)
         {
             case PageNumber.Page1:
-                nextButton.interactable = true;
                 pageNumber = PageNumber.Page2;
                 break;
 
             case PageNumber.Page2:
-                nextButton.interactable = true;
                 pageNumber = PageNumber.Page3;
                 break;
 
             case PageNumber.Page3:
-                nextButton.interactable = false;
                 break;
         }
 
@@ -64,21 +61,18 @@ public class Terrystruction : MonoBehaviour
         switch (pageNumber)
         {
             case PageNumber.Page1:
-                prevButton.interactable = false;
                 break;
 
             case PageNumber.Page2:
-                prevButton.interactable = true;
                 pageNumber = PageNumber.Page1;
                 break;
 
             case PageNumber.Page3:
-                prevButton.interactable = true;
                 pageNumber = PageNumber.Page2;
                 break;
         }
 
-        if (nextButton.interactable)
+        if (prevButton.interactable)
         {
             Establish();
         }
@@ -93,18 +87,24 @@ public class Terrystruction : MonoBehaviour
                 instructionImage.sprite = insImg[0];
                 titleText.text = "IM TERRY";
                 bodyText.text = "Heya lil missy,[insert exposition here], you got that?";
+                nextButton.interactable = true;
+                prevButton.interactable = false;
                 break;
 
             case PageNumber.Page2:
                 instructionImage.sprite = insImg[1];
                 titleText.text = "MOVE THEM CHEEKS";
                 bodyText.text = "Use WASD to move, press space to use them special cheeks of yours or any other legs you're using";
+                nextButton.interactable = true;
+                prevButton.interactable = true;
                 break;
 
             case PageNumber.Page3:
                 instructionImage.sprite = insImg[2];
                 titleText.text = "SHOOT'EM'UP";
                 bodyText.text = "Press left click to shoot using your head, press right click to shoot using your body";
+                nextButton.interactable = false;
+                prevButton.interactable = true;
                 break;
         }
     }
