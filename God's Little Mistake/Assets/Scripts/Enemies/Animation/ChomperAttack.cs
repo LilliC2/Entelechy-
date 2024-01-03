adfe1eac45c403110a3cf3c80b297806509cfa28
Lilli <lillichuawiwat@gmail.com>
2024-01-03 12:42:06 +1100

    Chomper Audio

--- God's Little Mistake/Assets/Scripts/Enemies/Animation/ChomperAttack.cs
+++ God's Little Mistake/Assets/Scripts/Enemies/Animation/ChomperAttack.cs
@@ -6,15 +6,30 @@ public class ChomperAttack : MonoBehaviour
 {
     EnemyChomper EC;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         EC = GetComponentInParent<EnemyChomper>();
     }
 
     public void Attack()
     {
-        EC.PerformAttack(EC.enemyStats.stats.fireRate);
+        EC.attackAudio.Play();
 
+    }
+    public void Hurt()
+    {
+        EC.hurtAudio.Play();
+
+    }
+
+    public void Walk()
+    {
+        EC.walkAudio.Play();
+
+    }
+
+    public void Death()
+    {
+        EC.deathAudio.Play();
     }
 }
